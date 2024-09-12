13330f22d6535d018bfc2de4f2a28200600d746d
Anderson Donaire <andersondonaire@hotmail.com>
2024-09-12 13:27:48 -0300

    02

--- admin/pg/homePg.php
+++ admin/pg/homePg.php
@@ -1,2 +1,17 @@
+<?php
+
+
+
+$nomePerfil = $sql->select("SELECT * FROM settings where id=1");
+$linkTwitter = $sql->select("SELECT * FROM settings where id=2");
+
+?>
 <h1>Home</h1>
 
+<form action="./" method="post" class="form-group">
+    <label class="label" for="nome_perfil">Nome Perfil</label>
+    <input class="form-control" type="text" name="nome_perfil" id="nome_perfil" value="<?=$nomePerfil['setting_value']?>">
+    <label class="label" for="link_twitter">Link Twitter</label>
+    <input class="form-control" type="text" name="link_twitter" id="link_twitter" value="<?=$linkTwitter['setting_value']?>">
+
+</form>
