c414a6b88a27b2ae420ccdd63a92a8b1fa06cb61
Anderson Donaire <andersondonaire@hotmail.com>
2024-09-12 11:32:46 -0300

    at

--- site.sql
+++ site.sql
@@ -1,58 +1,50 @@
 -- ----------------------------
 -- Table structure for imagens
 -- ----------------------------
-DROP TABLE IF EXISTS `imagens`;
-CREATE TABLE `imagens`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `titulo` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `caminho` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `fk` int(11) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+DROP TABLE IF EXISTS imagens;
+CREATE TABLE imagens  (
+  id int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  titulo varchar(255),
+  caminho varchar(255),
+  fk int(11),
+);
 
 -- ----------------------------
 -- Table structure for paginas
 -- ----------------------------
-DROP TABLE IF EXISTS `paginas`;
-CREATE TABLE `paginas`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `titulo` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `slug` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `conteudo` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `meta_description` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `meta_keywords` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE INDEX `slug`(`slug`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+DROP TABLE IF EXISTS paginas;
+CREATE TABLE paginas  (
+  id int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  titulo varchar(255),
+  slug varchar(255),
+  conteudo text,
+  meta_description varchar(255),
+  meta_keywords varchar(255),
+  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
 
 -- ----------------------------
 -- Table structure for settings
 -- ----------------------------
-DROP TABLE IF EXISTS `settings`;
-CREATE TABLE `settings`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `setting_key` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `setting_value` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE INDEX `setting_key`(`setting_key`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+DROP TABLE IF EXISTS settings;
+CREATE TABLE settings  (
+  id int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  setting_key varchar(100),
+  setting_value text
+);
 
 -- ----------------------------
 -- Table structure for usuarios
 -- ----------------------------
-DROP TABLE IF EXISTS `usuarios`;
-CREATE TABLE `usuarios`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `login` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `senha` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `nivel` int(11) NOT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
-
+DROP TABLE IF EXISTS usuarios;
+CREATE TABLE usuarios  (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  login varchar(50),
+  senha varchar(50),
+  nivel int(11) NOT NULL);
 -- ----------------------------
 -- Records of usuarios
 -- ----------------------------
-INSERT INTO `usuarios` VALUES (1, 'admin', 'lZSimac=', 0);
+INSERT INTO usuarios VALUES (1, 'admin', 'lZSimac=', 0);
 
