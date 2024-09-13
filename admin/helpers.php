ce5bf27e18ce3bf4a6077854e4b0d1b0488fe3e1
Anderson Donaire <andersondonaire@hotmail.com>
2024-09-13 13:21:50 -0300

    1321

--- admin/helpers.php
+++ admin/helpers.php
@@ -1,61 +1,66 @@
 <?php
 
+require_once "./connect.php";
 /**
  * Helpers [TIPO]
  *
  * @author Anderson Donaire - MASTER WEBS
  */
-class Helpers extends connect {
+class Helpers
+{
 
-    public static function encripta($string) {
-        $result = '';
-        $key = '05092024';
-        for ($i = 0; $i < strlen($string); $i++) {
-            $char = substr($string, $i, 1);
-            $keychar = substr($key, ($i % strlen($key)) - 1, 1);
-            $char = chr(ord($char) + ord($keychar));
-            $result .= $char;
-        }
-        return base64_encode($result);
+  public static function encripta($string)
+  {
+    $result = '';
+    $key = '05092024';
+    for ($i = 0; $i < strlen($string); $i++) {
+      $char = substr($string, $i, 1);
+      $keychar = substr($key, ($i % strlen($key)) - 1, 1);
+      $char = chr(ord($char) + ord($keychar));
+      $result .= $char;
     }
+    return base64_encode($result);
+  }
 
-    public static function decripta($string) {
-        $result = '';
-        $key = '05092024';
-        $string = base64_decode($string);
-        for ($i = 0; $i < strlen($string); $i++) {
-            $char = substr($string, $i, 1);
-            $keychar = substr($key, ($i % strlen($key)) - 1, 1);
-            $char = chr(ord($char) - ord($keychar));
-            $result .= $char;
-        }
-        return $result;
+  public static function decripta($string)
+  {
+    $result = '';
+    $key = '05092024';
+    $string = base64_decode($string);
+    for ($i = 0; $i < strlen($string); $i++) {
+      $char = substr($string, $i, 1);
+      $keychar = substr($key, ($i % strlen($key)) - 1, 1);
+      $char = chr(ord($char) - ord($keychar));
+      $result .= $char;
     }
+    return $result;
+  }
 
 
-/**
- * Exibe um modal de alerta com título, mensagem, classe de estilo e botões opcionais.
- *
- * Esta função gera um modal utilizando o Bootstrap 4.5.2 para exibir uma mensagem de alerta
- * ao usuário. O modal pode conter um título, uma mensagem, e pode ser estilizado com diferentes
- * classes de Bootstrap, como 'success', 'danger', 'warning', etc. Além disso, permite a inclusão
- * de botões personalizados no rodapé do modal.
- *
- * @param string|null $titulo O título do modal. Se não for fornecido, o título será nulo.
- * @param string $mensagem A mensagem de alerta que será exibida no corpo do modal.
- * @param string $classe A classe de Bootstrap que será aplicada ao modal para estilização. 
- *                       O valor padrão é 'success'.
- * @param array|null $arrBt Um array opcional de botões a serem exibidos no rodapé do modal. 
- *                          Cada botão deve ser um array associativo com as seguintes chaves:
- *                          - 'link' (string): O link de destino do botão.
- *                          - 'class' (string): A classe de estilo Bootstrap a ser aplicada ao botão.
- *                          - 'nome' (string): O texto exibido no botão.
- *
- * @return void
- */
-public static function alerta($titulo=null, $mensagem="", $classe='success', $arrBt = null) {
+  /**
+   * Exibe um modal de alerta com título, mensagem, classe de estilo e botões opcionais.
+   *
+   * Esta função gera um modal utilizando o Bootstrap 4.5.2 para exibir uma mensagem de alerta
+   * ao usuário. O modal pode conter um título, uma mensagem, e pode ser estilizado com diferentes
+   * classes de Bootstrap, como 'success', 'danger', 'warning', etc. Além disso, permite a inclusão
+   * de botões personalizados no rodapé do modal.
+   *
+   * @param string|null $titulo O título do modal. Se não for fornecido, o título será nulo.
+   * @param string $mensagem A mensagem de alerta que será exibida no corpo do modal.
+   * @param string $classe A classe de Bootstrap que será aplicada ao modal para estilização. 
+   *                       O valor padrão é 'success'.
+   * @param array|null $arrBt Um array opcional de botões a serem exibidos no rodapé do modal. 
+   *                          Cada botão deve ser um array associativo com as seguintes chaves:
+   *                          - 'link' (string): O link de destino do botão.
+   *                          - 'class' (string): A classe de estilo Bootstrap a ser aplicada ao botão.
+   *                          - 'nome' (string): O texto exibido no botão.
+   *
+   * @return void
+   */
+  public static function alerta($titulo = null, $mensagem = "", $classe = 'success', $arrBt = null)
+  {
 
-  $html = "
+    $html = "
       <script>
         document.addEventListener('DOMContentLoaded', function () {
           var myModal = new bootstrap.Modal(document.getElementById('modalAlerta'));
@@ -74,26 +79,27 @@ public static function alerta($titulo=null, $mensagem="", $classe='success', $ar
             </div>
             <div class=\"modal-footer\">";
 
-  if ($arrBt == null) {
+    if ($arrBt == null) {
       $html .= "<a href=\"javascript:history.go(-1)\" class=\"btn btn-primary\">Voltar</a>";
-  } else {
+    } else {
       foreach ($arrBt as $bt) {
-          $html .= "<a href=\"{$bt['link']}\" class=\"btn {$bt['class']}\">{$bt['nome']}</a>";
+        $html .= "<a href=\"{$bt['link']}\" class=\"btn {$bt['class']}\">{$bt['nome']}</a>";
       }
-  }
+    }
 
-  $html .= " </div>
+    $html .= " </div>
           </div>
         </div>
       </div>";
-  echo $html;
-}
+    echo $html;
+  }
 
-public static function alertaErro($mensagem, $urlVolta = null) {
+  public static function alertaErro($mensagem, $urlVolta = null)
+  {
 
-  $volta = $urlVolta ? $urlVolta : "javascript:history.go(-1)";
+    $volta = $urlVolta ? $urlVolta : "javascript:history.go(-1)";
 
-  echo "
+    echo "
       <script>
         document.addEventListener('DOMContentLoaded', function () {
           var myModal = new bootstrap.Modal(document.getElementById('modalErro'));
@@ -116,13 +122,14 @@ public static function alertaErro($mensagem, $urlVolta = null) {
           </div>
         </div>
       </div>";
-  exit();
-}
+    exit();
+  }
 
-public static function alertaSucesso($mensagem, $urlVolta = null) {
-  $volta = $urlVolta ? $urlVolta : "javascript:history.go(-1)";
+  public static function alertaSucesso($mensagem, $urlVolta = null)
+  {
+    $volta = $urlVolta ? $urlVolta : "javascript:history.go(-1)";
 
-  $html = "
+    $html = "
       <script>
         document.addEventListener('DOMContentLoaded', function () {
           var myModal = new bootstrap.Modal(document.getElementById('modalSucesso'));
@@ -141,24 +148,25 @@ public static function alertaSucesso($mensagem, $urlVolta = null) {
             </div>
             <div class=\"modal-footer\">";
 
-  if ($urlVolta != null) {
+    if ($urlVolta != null) {
       $html .= "<button type=\"button\" class=\"btn btn-secondary\" data-bs-dismiss=\"modal\">Fechar e ficar</button>";
       $html .= "<a href=\"{$volta}\" class=\"btn btn-primary\">Voltar</a>";
-  } else {
+    } else {
       $html .= "<button type=\"button\" class=\"btn btn-secondary\" data-bs-dismiss=\"modal\">Fechar</button>";
-  }
+    }
 
-  $html .= "
+    $html .= "
             </div>
           </div>
         </div>
       </div>";
-  echo $html;
-}
+    echo $html;
+  }
 
-public static function alertaConfirma($mensagem, $urlSim, $urlNao) {
+  public static function alertaConfirma($mensagem, $urlSim, $urlNao)
+  {
 
-  echo "
+    echo "
       <script>
         document.addEventListener('DOMContentLoaded', function () {
           var myModal = new bootstrap.Modal(document.getElementById('modalConfirma'));
@@ -182,29 +190,94 @@ public static function alertaConfirma($mensagem, $urlSim, $urlNao) {
           </div>
         </div>
       </div>";
-  exit();
-}
+    exit();
+  }
 
-  
-
-  public static function tiraAcento(string $string): string {
+  public static function tiraAcento(string $string): string
+  {
     // Substituição de acentos e caracteres especiais por suas versões simples.
     $ac = [
-        'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A', 'Å'=>'A', 'Æ'=>'AE', 'Ç'=>'C',
-        'È'=>'E', 'É'=>'E', 'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I', 'Ï'=>'I',
-        'Ð'=>'D', 'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O', 'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O',
-        'Ù'=>'U', 'Ú'=>'U', 'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'ss', 'à'=>'a',
-        'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a', 'å'=>'a', 'æ'=>'ae', 'ç'=>'c', 'è'=>'e',
-        'é'=>'e', 'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i', 'ï'=>'i', 'ð'=>'d',
-        'ñ'=>'n', 'ò'=>'o', 'ó'=>'o', 'ô'=>'o', 'õ'=>'o', 'ö'=>'o', 'ø'=>'o', 'ù'=>'u',
-        'ú'=>'u', 'û'=>'u', 'ü'=>'u', 'ý'=>'y', 'þ'=>'b', 'ÿ'=>'y', 'Ŕ'=>'R', 'ŕ'=>'r',
-        ' '=>'_', '+' => '_', '%' => '', '&' => 'e', '/' => '_', 'ª' => 'a', 'º' => 'o',
-        '\'' => '', '’' => '', '´' => '', '`' => '', '^' => ''
+      'À' => 'A',
+      'Á' => 'A',
+      'Â' => 'A',
+      'Ã' => 'A',
+      'Ä' => 'A',
+      'Å' => 'A',
+      'Æ' => 'AE',
+      'Ç' => 'C',
+      'È' => 'E',
+      'É' => 'E',
+      'Ê' => 'E',
+      'Ë' => 'E',
+      'Ì' => 'I',
+      'Í' => 'I',
+      'Î' => 'I',
+      'Ï' => 'I',
+      'Ð' => 'D',
+      'Ñ' => 'N',
+      'Ò' => 'O',
+      'Ó' => 'O',
+      'Ô' => 'O',
+      'Õ' => 'O',
+      'Ö' => 'O',
+      'Ø' => 'O',
+      'Ù' => 'U',
+      'Ú' => 'U',
+      'Û' => 'U',
+      'Ü' => 'U',
+      'Ý' => 'Y',
+      'Þ' => 'B',
+      'ß' => 'ss',
+      'à' => 'a',
+      'á' => 'a',
+      'â' => 'a',
+      'ã' => 'a',
+      'ä' => 'a',
+      'å' => 'a',
+      'æ' => 'ae',
+      'ç' => 'c',
+      'è' => 'e',
+      'é' => 'e',
+      'ê' => 'e',
+      'ë' => 'e',
+      'ì' => 'i',
+      'í' => 'i',
+      'î' => 'i',
+      'ï' => 'i',
+      'ð' => 'd',
+      'ñ' => 'n',
+      'ò' => 'o',
+      'ó' => 'o',
+      'ô' => 'o',
+      'õ' => 'o',
+      'ö' => 'o',
+      'ø' => 'o',
+      'ù' => 'u',
+      'ú' => 'u',
+      'û' => 'u',
+      'ü' => 'u',
+      'ý' => 'y',
+      'þ' => 'b',
+      'ÿ' => 'y',
+      'Ŕ' => 'R',
+      'ŕ' => 'r',
+      ' ' => '_',
+      '+' => '_',
+      '%' => '',
+      '&' => 'e',
+      '/' => '_',
+      'ª' => 'a',
+      'º' => 'o',
+      '\'' => '',
+      '’' => '',
+      '´' => '',
+      '`' => '',
+      '^' => ''
     ];
 
     // Remove os espaços em branco no início e no final da string.
     $string = trim($string);
-    
+
     // Transliteração dos caracteres especiais e acentuados.
     $string = strtr($string, $ac);
 
@@ -212,15 +285,15 @@ public static function alertaConfirma($mensagem, $urlSim, $urlNao) {
     $string = mb_strtolower($string, 'UTF-8');
 
     return $string;
-}
+  }
 
 
-    public static function getProxId($tabela) {
-//        include_once $_SESSION['dir'] . './set/config.php';
-//        $sql = new connect();
-        $proxId = parent::select("SHOW TABLE STATUS LIKE '{$tabela}'");
-        return $proxId['Auto_increment'];
-    }
+  public static function getSettings($setting_key)
+  {
+    $sql = new connect();
 
-
+    $consulta = $sql->select("SELECT * FROM settings WHERE setting_key =  '{$setting_key}'");
+    
+    return $consulta['setting_value'];
+  }
 }
--- admin/pg/homePg.php
+++ admin/pg/homePg.php
@@ -1,17 +1,9 @@
-<?php
-
-
-
-$nomePerfil = $sql->select("SELECT * FROM settings where id=1");
-$linkTwitter = $sql->select("SELECT * FROM settings where id=2");
-
-?>
 <h1>Home</h1>
-
 <form action="./" method="post" class="form-group">
     <label class="label" for="nome_perfil">Nome Perfil</label>
-    <input class="form-control" type="text" name="nome_perfil" id="nome_perfil" value="<?=$nomePerfil['setting_value']?>">
+    <input class="form-control" type="text" name="nome_perfil" id="nome_perfil" value="<?= Helpers::getSettings("nome_perfil"); ?>">
+
     <label class="label" for="link_twitter">Link Twitter</label>
-    <input class="form-control" type="text" name="link_twitter" id="link_twitter" value="<?=$linkTwitter['setting_value']?>">
+    <input class="form-control" type="text" name="link_twitter" id="link_twitter" value="">
 
 </form>
