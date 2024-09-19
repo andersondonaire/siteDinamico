ffbb1d66e63912cbfaa8c26016bc6f9979bb260b
Anderson Donaire <andersondonaire@hotmail.com>
2024-09-19 13:16:16 -0300

    1316

--- api.php
+++ api.php
@@ -11,10 +11,13 @@ $data = json_decode($requestPayload, true); // Decodifica o JSON para um array P
 //cabeçalho para que todos os retornos sejam em json
 header("Content-type: application/json; charset=utf-8");
 
+
 //retorna dados da página home
 if($data['acao'] == "getSettings"){
 
-    
+    $respostaDoBanco = $sql->selectFor("SELECT * FROM settings");
+
+    echo json_encode(["data" => $respostaDoBanco]);
 
 }
 
